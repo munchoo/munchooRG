4e4ed320dfb6ed6ecfafac2f44cf47191992e8b0
Kim Youngnam_mac <munchoo@gsretail.com>
2022-03-23 08:19:22 +0900

    last

--- Python/graph_api_demo.py
+++ Python/graph_api_demo.py
@@ -3,16 +3,17 @@ from numpy import heaviside
 import requests
 import webbrowser
 import msal as msal 
-from msal import PublicClientApplication
+from msal import ConfidentialClientApplication
 from sqlalchemy import false
+import urllib
   
 APPLICATION_ID = '312435a5-4dd6-40f8-a606-49bbb30c254d'
 CLIENT_SECRET = '6SH7Q~-bKmoBmiVopd-sRv6cjKwQd-HB9g2GL'
-authority_url = 'https://login.microsoftonline.com/common'
+authority_url = 'https://login.microsoftonline.com/gsretail.co.kr'
 base_url = 'https://graph.microsoft.com/v1.0/'
 
 
-SCOPES = ['User.Read', 'Chat.ReadWrite', 'Chat.Create']
+SCOPES = ['User.Read', 'Chat.ReadWrite', 'Chat.Create', 'Chat.Read', 'Chat.ReadBasic', 'Chat.ReadWrite', 'ChatMessage.Send', 'ChannelMessage.Send' ]
 
 # methon 1 : 
 client_instance = msal.ConfidentialClientApplication(
@@ -24,9 +25,15 @@ client_instance = msal.ConfidentialClientApplication(
 authorization_request_url = client_instance.get_authorization_request_url(SCOPES)   
 print(authorization_request_url)
 
-webbrowser.open(authorization_request_url, new=True)
 
-authorization_code = 'M.R3_BAY.d6390b82-7da0-28b7-ea1b-f60f18d4a183'
+# webbrowser.open(authorization_request_url, new=True)
+
+# my_response = input("Paste the full URL redirect here: ")
+# query_dict = urllib.parse.parse_qs(my_response)
+# print(query_dict)
+
+# authorization_code = query_dict['http://localhost:8000/get_auto_token?code']
+authorization_code = query_dict['http://localhost:8000/get_auto_token?code']
 access_token = client_instance.acquire_token_by_authorization_code(
      code=authorization_code,
      scopes=SCOPES
@@ -35,11 +42,23 @@ access_token = client_instance.acquire_token_by_authorization_code(
 print(access_token)
 
 access_token_id = access_token['access_token']
-headers = {'Authorization': 'Bearer ' + access_token_id}
-bodys = {'body': {'content': 'My name:'}}
+# headers = {'Authorization': 'Bearer ' + access_token_id}
+headers = { 
+'User-Agent' : 'python_tutorial/1.0',
+'Authorization': 'Bearer ' + access_token_id,
+'Accept' : 'application/json',
+'Content-Type' : 'application/json'
+}
+# bodys = {
+#     "body": {
+#         "content": "Hello world"
+#     }
+# }
 
 
-endpoint = base_url + 'chats/19:d43f046a47e749f78e5459cda683ac9a@thread.v2/messages'
-response = requests.post(endpoint, headers=headers, data=bodys)
+a = 'chats/19:bbd521b3-17df-44f9-9461-0e4751e750a4_dc6321ca-2f64-41fe-aa20-8d1f28465edd@unq.gbl.spaces'
+b = 'me/chats/'
+endpoint = base_url + b
+response = requests.get(endpoint, headers=headers)
 print(response.json())
  
---
+++ microsoft-authentication-library-for-python-dev/tests/test_authority.py
@@ -0,0 +1,123 @@
+import os
+
+from msal.authority import *
+from tests import unittest
+from tests.http_client import MinimalHttpClient
+
+
+@unittest.skipIf(os.getenv("TRAVIS_TAG"), "Skip network io during tagged release")
+class TestAuthority(unittest.TestCase):
+
+    def _test_given_host_and_tenant(self, host, tenant):
+        c = MinimalHttpClient()
+        a = Authority('https://{}/{}'.format(host, tenant), c)
+        self.assertEqual(
+            a.authorization_endpoint,
+            'https://{}/{}/oauth2/v2.0/authorize'.format(host, tenant))
+        self.assertEqual(
+            a.token_endpoint,
+            'https://{}/{}/oauth2/v2.0/token'.format(host, tenant))
+        c.close()
+
+    def _test_authority_builder(self, host, tenant):
+        c = MinimalHttpClient()
+        a = Authority(AuthorityBuilder(host, tenant), c)
+        self.assertEqual(
+            a.authorization_endpoint,
+            'https://{}/{}/oauth2/v2.0/authorize'.format(host, tenant))
+        self.assertEqual(
+            a.token_endpoint,
+            'https://{}/{}/oauth2/v2.0/token'.format(host, tenant))
+        c.close()
+
+    def test_wellknown_host_and_tenant(self):
+        # Assert all well known authority hosts are using their own "common" tenant
+        for host in WELL_KNOWN_AUTHORITY_HOSTS:
+            self._test_given_host_and_tenant(host, "common")
+
+    def test_wellknown_host_and_tenant_using_new_authority_builder(self):
+        self._test_authority_builder(AZURE_PUBLIC, "consumers")
+        self._test_authority_builder(AZURE_CHINA, "organizations")
+        self._test_authority_builder(AZURE_US_GOVERNMENT, "common")
+
+    @unittest.skip("As of Jan 2017, the server no longer returns V1 endpoint")
+    def test_lessknown_host_will_return_a_set_of_v1_endpoints(self):
+        # This is an observation for current (2016-10) server-side behavior.
+        # It is probably not a strict API contract. I simply mention it here.
+        less_known = 'login.windows.net'  # less.known.host/
+        v1_token_endpoint = 'https://{}/common/oauth2/token'.format(less_known)
+        a = Authority(
+            'https://{}/common'.format(less_known), MinimalHttpClient())
+        self.assertEqual(a.token_endpoint, v1_token_endpoint)
+        self.assertNotIn('v2.0', a.token_endpoint)
+
+    def test_unknown_host_wont_pass_instance_discovery(self):
+        _assert = getattr(self, "assertRaisesRegex", self.assertRaisesRegexp)  # Hack
+        with _assert(ValueError, "invalid_instance"):
+            Authority('https://example.com/tenant_doesnt_matter_in_this_case',
+                      MinimalHttpClient())
+
+    def test_invalid_host_skipping_validation_can_be_turned_off(self):
+        try:
+            Authority(
+                'https://example.com/invalid',
+                MinimalHttpClient(), validate_authority=False)
+        except ValueError as e:
+            if "invalid_instance" in str(e):  # Imprecise but good enough
+                self.fail("validate_authority=False should turn off validation")
+        except:  # Could be requests...RequestException, json...JSONDecodeError, etc.
+            pass  # Those are expected for this unittest case
+
+
+class TestAuthorityInternalHelperCanonicalize(unittest.TestCase):
+
+    def test_canonicalize_tenant_followed_by_extra_paths(self):
+        _, i, t = canonicalize("https://example.com/tenant/subpath?foo=bar#fragment")
+        self.assertEqual("example.com", i)
+        self.assertEqual("tenant", t)
+
+    def test_canonicalize_tenant_followed_by_extra_query(self):
+        _, i, t = canonicalize("https://example.com/tenant?foo=bar#fragment")
+        self.assertEqual("example.com", i)
+        self.assertEqual("tenant", t)
+
+    def test_canonicalize_tenant_followed_by_extra_fragment(self):
+        _, i, t = canonicalize("https://example.com/tenant#fragment")
+        self.assertEqual("example.com", i)
+        self.assertEqual("tenant", t)
+
+    def test_canonicalize_rejects_non_https(self):
+        with self.assertRaises(ValueError):
+            canonicalize("http://non.https.example.com/tenant")
+
+    def test_canonicalize_rejects_tenantless(self):
+        with self.assertRaises(ValueError):
+            canonicalize("https://no.tenant.example.com")
+
+    def test_canonicalize_rejects_tenantless_host_with_trailing_slash(self):
+        with self.assertRaises(ValueError):
+            canonicalize("https://no.tenant.example.com/")
+
+
+@unittest.skipIf(os.getenv("TRAVIS_TAG"), "Skip network io during tagged release")
+class TestAuthorityInternalHelperUserRealmDiscovery(unittest.TestCase):
+    def test_memorize(self):
+        # We use a real authority so the constructor can finish tenant discovery
+        authority = "https://login.microsoftonline.com/common"
+        self.assertNotIn(authority, Authority._domains_without_user_realm_discovery)
+        a = Authority(authority, MinimalHttpClient(), validate_authority=False)
+
+        try:
+            # We now pretend this authority supports no User Realm Discovery
+            class MockResponse(object):
+                status_code = 404
+            a.user_realm_discovery("john.doe@example.com", response=MockResponse())
+            self.assertIn(
+                "login.microsoftonline.com",
+                Authority._domains_without_user_realm_discovery,
+                "user_realm_discovery() should memorize domains not supporting URD")
+            a.user_realm_discovery("john.doe@example.com",
+                response="This would cause exception if memorization did not work")
+        finally:  # MUST NOT let the previous test changes affect other test cases
+            Authority._domains_without_user_realm_discovery = set([])
+
