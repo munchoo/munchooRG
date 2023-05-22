a1837c64bd5162c1c99605e999607156b985ad5f
munchoo <munchoo@live.com>
2023-05-22 11:56:04 +0900

    0522

--- Python/session_jumgyungsi.py
+++ Python/session_jumgyungsi.py
@@ -1,2 +1,21 @@
 import requests
-s = 
+from bs4 import BeautifulSoup
+import gzip
+
+
+s = requests.session()
+cookies2 = 'WMONID=ZNNT1AqXS1L; JSESSIONID=0001vaHfWhiC-NLhdFRWimkfnIE:1b3rmbf21'
+headers2 ={'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7', 'Accept-Encoding':'gzip, deflate', 'Accept-Language':'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7', 'Cookie':cookies2, 'Host':'cvsscn.gsretail.com', 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'} 
+parm = {'fileNm':'GD_8802521894760_001.jpg'}
+
+req = s.get('http://cvsscn.gsretail.com/cssc/index.html', headers=headers2)
+
+
+
+# html 소스가져오기
+html = req.text
+header = req.headers
+status = req.status_code
+soup = BeautifulSoup(req.text, 'html.parser')
+print(soup)
+
--- SQL_GS/SQL_만들기/공결체크하기.sql
+++ SQL_GS/SQL_만들기/공결체크하기.sql
@@ -1,10 +1,10 @@
-
 select a11.OPER_DT,
           a11.ORIGIN_BIZPL_CD,  
           a12.STORENM, 
-         row_number() over (partition by a11.ORIGIN_BIZPL_CD, a11.GOODS_CD order  by a11.OPER_DT  asc) as hihi, 
+         row_number() over (partition by a11.ORIGIN_BIZPL_CD, a11.GOODS_CD order  by a11.OPER_DT  asc) as commu_OUTDAY, 
           a11.GOODS_SP,
-          a11.GOODS_CD,
+          a11.GOODS_CD, 
+          a13.GOODNM, 
           a11.GOODS_OPT_SP,
           a11.TARGET_GOODS_CNT,
           a11.OUTSTK_YN,
@@ -16,8 +16,11 @@ select a11.OPER_DT,
           a11.INSERT_DTTM
 from   LGMJVDP.TS_OP_OUTSTK_SKU a11 
 join LGMJVDP.TB_STORE_DM a12 
-on trim(a11.BIZPL_CD) = 'V'||a12.STORECD 
+on trim(a11.BIZPL_CD) = 'V'||a12.STORECD  
+join LGMJVDP.TB_GOOD_DM a13
+on trim(a11.GOODS_CD) = a13.GOODCD
 where a12.RGNCD in ('54') 
-and OPER_DT between '20230401' and '20230431' 
-and a11.GOODS_CD is not null 
-  
+and a12.TEAMCD in ('5402') 
+and OPER_DT between '20230501' and '20230530'
+and a11.GOODS_CD is not null  
+order by 4 desc
